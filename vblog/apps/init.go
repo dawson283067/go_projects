a636a7726b5ed0d8d6628001ee79711ce4b8e49b
Dawson <wdq.vip@gmail.com>
2024-02-04 13:10:09 +0800

    补充API的具体实现

--- vblog/apps/init.go
+++ vblog/apps/init.go
@@ -7,4 +7,5 @@ import (
 	_ "github.com/go_projects/vblog/apps/token/impl"
 	_ "github.com/go_projects/vblog/apps/user/impl"
 	_ "github.com/go_projects/vblog/apps/blog/impl"
+	_ "github.com/go_projects/vblog/apps/blog/api"
 )
