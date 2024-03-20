6557b0d275c8bb9598fec13b2319e7cc3a171bc9
dawson <wdq.vip@gmail.com>
2024-03-20 12:21:04 -0400

    补充UI组件库

---
+++ vblog/ui/src/main.js
@@ -0,0 +1,23 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+// 引入UI组件库
+import ArcoVue from '@arco-design/web-vue';
+// Icon是一个独立的组件库，需要单独引入
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
+// 同时引入arco.design的样式（该样式会覆盖之前的样式）
+import '@arco-design/web-vue/dist/arco.css';
+app.use(ArcoVue)
+app.use(ArcoVueIcon)
+
+app.mount('#app')
